7066de051a57b9fa2e3a0a33aa2b01fc9949f777
AndrewNikkk <nikitin.andey2005@mail.ru>
2025-12-04 02:48:54 +0300

    fix settings frontend

--- frontend/src/components/ProfileHeader.tsx
+++ frontend/src/components/ProfileHeader.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './ProfileHeader.css';
 import logoImage from '../assets/Logo.svg';
 import settingsIcon from '../assets/Settings.svg';
 import chatIcon from '../assets/Chat.svg';
 
 export const ProfileHeader: React.FC = () => {
+  const location = useLocation();
+  const isSettingsPage = location.pathname === '/settings';
+
   return (
     <header className="profile-header">
-      <div className="profile-header-content">
+      <div className={`profile-header-content ${isSettingsPage ? 'settings-page-header' : ''}`}>
         <Link to="/" className="profile-header-logo">
           <img src={logoImage} alt="Logo" className="profile-header-logo-icon" />
           <span className="profile-header-logo-text">Blur</span>
         </Link>
         
         <div className="profile-header-buttons">
-          <Link to="/settings" className="profile-header-btn profile-btn">
-            <img src={settingsIcon} alt="Settings" className="settings-btn-icon" />
-            <span className="profile-btn-text">Настройки</span>
-          </Link>
+          {!isSettingsPage && (
+            <Link to="/settings" className="profile-header-btn profile-btn">
+              <img src={settingsIcon} alt="Settings" className="settings-btn-icon" />
+              <span className="profile-btn-text">Настройки</span>
+            </Link>
+          )}
           <button className="profile-header-btn logout-btn">
             <img src={chatIcon} alt="Chat" className="chat-btn-icon" />
             <span className="logout-btn-text">Открыть чат</span>
